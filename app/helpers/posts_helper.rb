b439a1a8b87636d52267075d1d36d5fdc485fe05
Jeff Beal <jrbeal@cox.net>
2015-04-23 14:34:27 -0600

    Posts helper, level/offspring/status_color methods

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,36 @@
 module PostsHelper
+  def status_color(post)
+    case post.kind
+      when Post::COMMENT
+        "lightblue"
+      when Post::INITIATOR
+        "lightyellow"
+      when Post::OPINION
+        case post.status
+          when Post::TRUE
+            "lightgreen"
+          when Post::FALSE
+            "lightred"
+        end
+    end
+  end
+
+  def offspring(post)
+    case post.kind
+      when Post::COMMENT
+        "Replies (#{post.children_comments} / #{post.offspring_comments}), "
+      when Post::INITIATOR
+        "Opinions (#{post.children_opinions} / #{post.offspring_opinions}), Comments (#{post.children_comments} / #{post.offspring_comments}), "
+      when Post::OPINION
+        "Objections (#{post.children_opinions} / #{post.offspring_opinions}), Comments (#{post.children_comments} / #{post.offspring_comments}), "
+    end
+  end
+
+  def level(post)
+    case post.kind
+      when Post::OPINION
+        "Level = #{post.level},"
+    end
+  end
+
 end

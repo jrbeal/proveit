5208fce5bd55e6489579ec502a27cfb3efd28ba9
Jeff Beal <jrbeal@cox.net>
2015-04-23 14:36:08 -0600

    Preliminary post show endpoint

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,9 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @post = Post.find(params[:id])
+
+    puts @post.inspect
   end
 
   # GET /posts/new

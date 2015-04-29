c43caa714a3518d813615c602d0cd3aef3aa9db9
Jeff Beal <jrbeal@cox.net>
2015-04-29 13:46:26 -0600

    Periodic commit. Eveything's working

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -37,17 +37,17 @@ module PostsHelper
     case post.kind
       when Post::INITIATOR
         raw ("
-        #{radio_button_tag('kidtype', 'opinions', true)}
-        #{label_tag('kidtype', 'Opinions')}
-        #{radio_button_tag('kidtype', 'comments')}
-        #{label_tag('kidtype', 'Comments')}
+        #{radio_button_tag 'kidtype', 'opinion', true}
+        #{label_tag 'kidtype', 'Opinions'}
+        #{radio_button_tag 'kidtype', 'comment'}
+        #{label_tag 'kidtype', 'Comments'}
         ")
       when Post::OPINION
         raw ("
-        #{radio_button_tag('kidtype', 'opinions', true)}
-        #{label_tag('kidtype', 'Objections')}
-        #{radio_button_tag('kidtype', 'comments')}
-        #{label_tag('kidtype', 'Comments')}
+        #{radio_button_tag 'kidtype', 'opinion', true}
+        #{label_tag 'kidtype', 'Objections'}
+        #{radio_button_tag 'kidtype', 'comment'}
+        #{label_tag 'kidtype', 'Comments'}
         ")
     end
   end

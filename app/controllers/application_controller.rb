81eae760fc0736f63245edb30cfbb863e1b608c0
Jeff Beal <jrbeal@cox.net>
2015-07-01 21:08:44 -0600

    minor mods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,8 @@ class ApplicationController < ActionController::Base
 			length = f.name.length if f.name.length > length
 		end
 
-		@divider = "-" * (length * 1.5)
+		@divider = "-" * (length * 1.5)									# A hack to make the divider roughly as long
+																										# as the longest filter name.
 
 		if current_prover.cur_filter.present?
 			@filter = current_prover.cur_filter 					# Get user's current filter (should exist)
@@ -52,13 +53,13 @@ class ApplicationController < ActionController::Base
 		@posts = Post.where(id: ids).order(updated_at: :desc) if ids.length > 0
 
 		# Now start filtering...
-																				# Of the remaining posts, eliminate those outside the specified time range
+																				# First eliminate those outside the specified time range
 		@posts = @posts.where(:updated_at => 1.day.ago..Time.now) if @filter.today
 		@posts = @posts.where(:updated_at => 1.week.ago..Time.now) if @filter.last_week
 		@posts = @posts.where(:updated_at => 1.month.ago..Time.now) if @filter.last_month
 		@posts = @posts.where(:updated_at => 1.year.ago..Time.now) if @filter.last_year
 
-		if @filter.following								# Of the remaining posts, eliminate those that are NOT by someone being followed
+		if @filter.following								# now eliminate those that are NOT by someone being followed
 			ids = []
 			Follow.where(owner: current_prover).each do |f|
 				Post.where(prover_id: f.follows.id).each do |p|
@@ -68,7 +69,7 @@ class ApplicationController < ActionController::Base
 			@posts = @posts.where(id: ids).order(updated_at: :desc)
 		end
 
-		if @filter.bookmarks								# Of the remaining posts, eliminate those that are NOT bookmarks
+		if @filter.bookmarks								# now eliminate those that are NOT bookmarks
 			ids = []
 			Bookmark.where(owner: current_prover).each do |b|
 				ids.push b.post.id
@@ -76,7 +77,7 @@ class ApplicationController < ActionController::Base
 			@posts = @posts.where(id: ids).order(updated_at: :desc)
 		end
 
-		if @filter.lone_wolf								# Of the remaining posts, eliminate those that are NOT Lone wolf
+		if @filter.lone_wolf								# now eliminate those that are NOT Lone wolf
 			ids = []
 			@posts.each do |p|
 				ids.push p.id if p.topic.lone_wolf
@@ -84,7 +85,7 @@ class ApplicationController < ActionController::Base
 			@posts = @posts.where(id: ids).order(updated_at: :desc)
 		end
 
-		if @filter.private									# Of the remaining posts, eliminate those that are NOT private...
+		if @filter.private									# now eliminate those that are NOT private...
 			ids = []
 			if @filter.public_viewing					# ... and do NOT allow public viewing
 				if @filter.public_comments			# ... and do NOT allow public comments
@@ -105,8 +106,8 @@ class ApplicationController < ActionController::Base
 		end
 
 		ids = []
-		@posts.each do |p|									# Of the remaining posts, eliminate those that exclude current user
-			ids.push p.id unless (p.topic.private && (!p.topic.public_viewing && !(p.team_member?(current_prover) && !current_prover.administrator)))
+		@posts.each do |p|									# now eliminate private posts that exclude current user...
+			ids.push p.id unless p.topic.private && !(p.topic.public_viewing || p.team_member?(current_prover) || current_prover.administrator)
 		end
 		@posts = @posts.where(id: ids).order(updated_at: :desc)
 
@@ -118,7 +119,7 @@ class ApplicationController < ActionController::Base
 		@posts = @posts.where(level: 0) if @filter.level_zero
 		@posts = @posts.where.not(level: 0) if @filter.level_nonzero
 
-		@posts.each do |p|									# Of the remaining posts, create team lists (if they are private)
+		@posts.each do |p|									# One final pass to build the team lists (for private posts)
 			p.create_team_lists
 		end
 	end

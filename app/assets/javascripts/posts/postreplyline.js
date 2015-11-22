bb075e02447aa25326a4eb7276a647c078dad0a2
Jeff Beal <jrbeal@cox.net>
2015-11-22 18:42:35 -0500

    Debuggery

--- app/assets/javascripts/posts/postreplyline.js
+++ app/assets/javascripts/posts/postreplyline.js
@@ -6,7 +6,6 @@ $(function() {
 			console.log("Got prover for setting kidtype (" + resp.currentprover.kidtype + ") and offspring style (" + resp.currentprover.offspring_style + ").");
 			$('input[name="kidtype"][value="' + resp.currentprover.kidtype + '"]').click();
 
-			console.log($('input[name="kidtype"][value="' + resp.currentprover.kidtype + '"]'));
 			switch (resp.currentprover.offspring_style) {
 				case 'collapsed':
 					$('#expand').show();

fddc319d1e33fc74cfe1206f075dc5d089357f82
Jeff Beal <jrbeal@cox.net>
2016-10-13 21:42:29 -0400

    Minutiae

--- app/assets/javascripts/posts/objectionfallacylist.js
+++ app/assets/javascripts/posts/objectionfallacylist.js
@@ -2,17 +2,13 @@ $(function() {
 	$('#objectionfallacylist').on("click", function () {
 		var Id = $('#objectionfallacylist option:selected').val();
 		var name = $('#objectionfallacylist option:selected').text();
-		console.log(name, Id);
 		if (name.search("--->") >= 0 || name.search("<---") >= 0) {
-			console.log("Selected filter folder.");
 			$.ajax({
 				url: '/fallacyfolders/contents/' + Id + '.json?' + $.param({"id": Id}),
 				type: 'GET',
 				success: function (resp) {
 					var joined = resp.joined;
 					var parentid = resp.parentid;
-					console.log(joined);
-					console.log(parentid);
 					$('#objectionfallacylist option').remove();
 					if (parentid) {
 						$('#objectionfallacylist').append($("<option />").val(parentid).text("<--- Back"));
@@ -27,14 +23,12 @@ $(function() {
 				}
 			});
 		} else if (name.length > 0) {
-			console.log("Selected fallacy.");
 			$("#fallacy_details_id").val(Id);
 			$.ajax({
 				url: '/fallacies/' + Id + '.json?' + $.param({"id": Id}),
 				type: 'GET',
 				success: function (resp) {
 					var fallacy = resp.fallacy;
-					console.log(fallacy);
 					$('#objectionopinion').val(fallacy.opinion);
 					$('#objectionsupport').val(fallacy.support);
 					$('#objectionurl').val(fallacy.url);
--- app/assets/javascripts/posts/objectiontype.js
+++ app/assets/javascripts/posts/objectiontype.js
@@ -4,21 +4,44 @@ $(function() {
 		var $tgt = $(e.target);
 		if ($tgt.val() == "manual") {
 			$('#objectionfallacy').hide();
+			$('#objectionopinion').val("");
+			$('#objectionsupport').val("");
+			$('#objectionurl').val("");
 		} else if ($tgt.val() == "fallacy") {
 			$('#objectionfallacy').show();
+			$('#objectionopinion').val("");
+			$('#objectionsupport').val("");
+			$('#objectionurl').val("");
 		} else if ($tgt.val() == "inconclusive") {
 			$('#objectionfallacy').hide();
+			$('#objectionopinion').val("This objection fails to draw a conclusion.");
+			$('#objectionsupport').val("");
+			$('#objectionurl').val("");
 		} else if ($tgt.val() == "ambiguous") {
 			$('#objectionfallacy').hide();
+			$('#objectionopinion').val("This objection is unclear.");
+			$('#objectionsupport').val("");
+			$('#objectionurl').val("");
 		} else if ($tgt.val() == "irrelevant") {
 			$('#objectionfallacy').hide();
+			$('#objectionopinion').val("This objection misses the point.");
+			$('#objectionsupport').val("");
+			$('#objectionurl').val("");
 		} else if ($tgt.val() == "duplicate") {
 			$('#objectionfallacy').hide();
+			$('#objectionopinion').val("This objection already exists");
+			$('#objectionsupport').val("");
+			$('#objectionurl').val("");
+		} else if ($tgt.val() == "unsubstantiated") {
+			$('#objectionfallacy').hide();
+			$('#objectionopinion').val("This objection lacks rationale or evidence.");
+			$('#objectionsupport').val("");
+			$('#objectionurl').val("");
 		} else if ($tgt.val() == "mediationrequest") {
 			$('#objectionfallacy').hide();
+			$('#objectionopinion').val("Requesting mediation.");
+			$('#objectionsupport').val("Describe impass here.");
+			$('#objectionurl').val("");
 		}
-		$('#objectionopinion').val("");
-		$('#objectionsupport').val("");
-		$('#objectionurl').val("");
 	});
 });

e0fa8dd531303f3c634fadac4db9f338f1b21c6e
Jeff Beal <jrbeal@cox.net>
2016-11-23 20:31:26 -0500

    Fixed filter in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-DECAY_FACTOR = 'decay_factor'
+CONFIG_NAME = 'decay_factor'
 HALF_LIFE_52 = 0.98675869426		# 52 week half life
 HALF_LIFE_26 = 0.97369272069		# 26 week half life
 HALF_LIFE_13 = 0.94807751434		# 13 week half life
 HALF_LIFE_4 = 0.84089641525		# 4 week half life
 HALF_LIFE_1 = 0.50000000000		# 1 week half life
 
-Siteconfig.create(:name => DECAY_FACTOR, :floatvalue => HALF_LIFE_26) unless Siteconfig.find_by :name => DECAY_FACTOR
+Siteconfig.create(:name => CONFIG_NAME, :floatvalue => HALF_LIFE_26) unless Siteconfig.find_by :name => CONFIG_NAME
 
 Filter.create(:name => "All", :sitedefault => true, :opinions => true, :has_no_parent => true, :level_zero => true)
